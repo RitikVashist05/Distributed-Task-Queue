624c7c912ed976fce22ce75551dc2972b2aba9b2
RitikVashist05 <ritikvashist05@gmail.com>
2024-10-11 15:29:06 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,24 +4,32 @@
 #include <cstdlib>
 
 int main() {
+    // Initialize server and client
     Server server;
     Client client;
 
+    // Start the server in a separate thread
     thread serverThread(&Server::start, &server);
     serverThread.detach();
 
+    // Input number of tasks to submit
     cout << "Enter number of tasks: ";
     int numTasks;
     cin >> numTasks;
 
+    // Submit multiple tasks with priorities
     for (int i = 0; i < numTasks; ++i) {
         string task;
+        int priority;
         cout << "Enter task description: ";
         cin >> task;
-        client.submitTask(task);
+        cout << "Enter task priority (0 = highest priority): ";
+        cin >> priority;
+        client.submitTask(task, priority);
     }
 
-    this_thread::sleep_for(chrono::seconds(20));
+    // Give time for tasks to process
+    this_thread::sleep_for(chrono::seconds(30));
 
     return 0;
 }
