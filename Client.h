78f3fa8a321fa1dc21ddb59f3a559f1993e04874
RitikVashist05 <ritikvashist05@gmail.com>
2024-10-11 15:30:57 +0530

    Update Client.h

--- Client.h
+++ Client.h
@@ -11,21 +11,45 @@ using namespace std;
 
 class Client {
 public:
-    void submitTask(const string& task) {
+    // Submit task to the server with a description
+    void submitTask(const string& task, int priority) {
         int sock = 0;
         struct sockaddr_in serv_addr;
 
+        // Create socket
         if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
             cout << "Socket creation error" << endl;
             return;
         }
 
         serv_addr.sin_family = AF_INET;
-        serv_addr.sin_port = htons(8080);
+        serv_addr.sin_port = htons(8080);  // Server port
 
+        // Convert IP address from text to binary
         if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
             cout << "Invalid address" << endl;
             return;
+        }
+
+        // Connect to the server
+        if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+            cout << "Connection failed" << endl;
+            return;
+        }
+
+        // Send task to the server with the task priority
+        string message = to_string(priority) + ":" + task;
+        send(sock, message.c_str(), message.length(), 0);
+
+        // Debugging output
+        cout << "Submitting task: " << task << " with priority: " << priority << endl;
+
+        close(sock);
+    }
+};
+
+#endif
+
         }
 
         if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
