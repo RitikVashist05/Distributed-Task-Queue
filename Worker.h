08fc7f15cb53ace912578063c7e791eaba874679
RitikVashist05 <ritikvashist05@gmail.com>
2024-10-11 20:50:26 +0530

    Update Worker.h

--- Worker.h
+++ Worker.h
@@ -3,18 +3,14 @@
 
 #include <iostream>
 #include <thread>
-
-using namespace std;
+#include <chrono>
+#include <string>
 
 class Worker {
 public:
-    // Simulate task processing
-    void processTask(const string& task) {
-        cout << "Worker processing task: " << task << endl;
-        this_thread::sleep_for(chrono::seconds(3));  // Simulate processing time
-        cout << "Worker completed task: " << task << endl;
-    }
+    void processTask(const std::string& task);
 };
 
 #endif // WORKER_H
+
 
