b33a5dac343a13c1ba7665f32a978e0b3293b292
RitikVashist05 <ritikvashist05@gmail.com>
2024-10-11 15:38:44 +0530

    Update Worker.h

--- Worker.h
+++ Worker.h
@@ -8,11 +8,13 @@ using namespace std;
 
 class Worker {
 public:
+    // Simulate task processing
     void processTask(const string& task) {
         cout << "Worker processing task: " << task << endl;
-        this_thread::sleep_for(chrono::seconds(3));
+        this_thread::sleep_for(chrono::seconds(3));  // Simulate processing time
         cout << "Worker completed task: " << task << endl;
     }
 };
 
-#endif
+#endif // WORKER_H
+
